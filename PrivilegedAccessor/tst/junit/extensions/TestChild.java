625de4b5867e6d4101578e00825c74502aa64bd7
sebidietrich <sebidietrich@303768d5-1ccc-c650-f77b-ce2428634138>
2005-01-28 16:45:56 +0000

    initial checkin

---
+++ PrivilegedAccessor/tst/junit/extensions/TestChild.java
@@ -0,0 +1,30 @@
+package junit.extensions;
+
+/**
+ * Test class with private methods to invoke via PrivilegedAccessor
+ */
+public class TestChild extends TestParent {
+    private int privateNumber;
+
+    private TestChild(String name, Integer number) {
+        super(name);
+        this.privateNumber = number.intValue();
+    }
+
+    public TestChild(String name) {
+        this(name, new Integer(8));
+    }
+
+    private int getNumber() {
+        return this.privateNumber;
+    }
+
+    private void setNumber(int number) {
+        this.privateNumber = number;
+    }
+
+    private void setData(String name, Integer number) {
+        setName(name);
+        privateNumber = number.intValue();
+    }
+}
---
+++ PrivilegedAccessor/tst/junit/extensions/TestParent.java
@@ -0,0 +1,43 @@
+package junit.extensions;
+
+/**
+ * Test class with private methods to invoke via PrivilegedAccessor
+ */
+public class TestParent {
+    private String privateName;
+
+    private static int privateStaticNumber;
+
+    public TestParent(String name) {
+        this.privateName = name;
+        privateStaticNumber = 1;
+    }
+
+    private TestParent() {
+        this.privateName = "Charlie";
+    }
+
+    private String getName() {
+        return this.privateName;
+    }
+
+    protected void setName(String newName) {
+        this.privateName = newName;
+    }
+
+    private static void setStaticNumber(int number) {
+        TestParent.privateStaticNumber = number;
+    }
+
+    public boolean equals(Object other) {
+        if (!(other instanceof TestParent))
+            return false;
+
+        TestParent otherTestParent = (TestParent) other;
+
+        if (this.privateName.equals(otherTestParent.privateName))
+            return true;
+
+        return false;
+    }
+}
