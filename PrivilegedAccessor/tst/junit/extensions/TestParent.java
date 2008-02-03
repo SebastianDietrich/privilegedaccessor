a0f866f8fc160ecbe1b9822bc18d4d7648ed7ba5
sebidietrich <sebidietrich@303768d5-1ccc-c650-f77b-ce2428634138>
2008-02-03 13:45:57 +0000

    merged 1.2 down here

--- PrivilegedAccessor/tst/junit/extensions/TestParent.java
+++ PrivilegedAccessor/tst/junit/extensions/TestParent.java
@@ -24,6 +24,11 @@ public class TestParent {
     protected void setName(String newName) {
         this.privateName = newName;
     }
+    
+    /** overloading setName(String) **/
+    private void setName() {
+        this.privateName = "Chaplin";
+    }
 
     private static void setStaticNumber(int number) {
         TestParent.privateStaticNumber = number;
