a018596358dec710daa264472c42a9f009e6879e
sebidietrich@gmail.com <sebidietrich@gmail.com@303768d5-1ccc-c650-f77b-ce2428634138>
2012-03-03 11:51:18 +0000

    [Issue 13] added license infos

--- PrivilegedAccessor/src/test/java/junit/extensions/PrivilegedAccessorTest.java
+++ PrivilegedAccessor/src/test/java/junit/extensions/PrivilegedAccessorTest.java
@@ -1,12 +1,29 @@
+/*
+ * Copyright 2004-2012 Sebastian Dietrich (Sebastian.Dietrich@e-movimento.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package junit.extensions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.lang.reflect.InvocationTargetException;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
-import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,7 +32,7 @@ import org.junit.Test;
 /**
  * Tests for the class <code>PrivilegedAccessor</code>.
  */
-public class PrivilegedAccessorTest extends TestCase {
+public class PrivilegedAccessorTest {
 
    /**
     * An instance of a test-subclass.
